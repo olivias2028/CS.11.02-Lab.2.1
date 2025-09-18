c9afdbec13f45b57aa0084c37909e48f2173ebb6
Olivia <olivias2028@student.cis.edu.hk>
2025-09-18 08:19:10 +0800

    Hello, Boba!

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Olivia
 *****************************************************************************************************
 */
 
@@ -11,21 +11,48 @@ public class Main {
     public static void main(String[] args) {
 
     }
-
-    // 1. add
+    public static int add(int a, int b){
+        return  a+b;
+    }
     
-    // 2. add
+    public static int add(int a, int b, int c, int d){
+        int ab = add(a,b);
+        int cd = add(c,d);
+        return add(ab,cd);
+    }
 
-    // 3. morningGreeting
-
-    // 4. afternoonGreeting
-
-    // 5. triple
-
-    // 6. half
-
-    // 7. roundPositiveValueToNearestInteger
+    public static String morningGreeting(String name){
+        return ("早上好, " + name + "!");
+    }
+    public static String afternoonGreeting(String name){
+        return  ("下午好, " + name + "!" );
+    }
+    public static String triple(String name){
+        return (name + name + name);
+    }
+    public static double half(double a){
+        return a/2;
+    }
+    public static double roundPositiveValueToNearestInteger(double a){
+        int integerpart = (int) a;
+        double decimalpart = a - integerpart;
+        if (decimalpart < 0.5) {
+            return integerpart;
+        }
+        else {
+            return integerpart + 1;
+        }
+    }
 
     // 8. roundNegativeValueToNearestInteger
-
+    public static double roundNegativeValueToNearestInteger(double a){
+        int integerpart = (int) a;
+        double decimalpart = a - integerpart;
+        if (decimalpart > -0.5){
+            return integerpart;
+        }
+        else {
+            return integerpart -1;
+        }
+    }
 }
